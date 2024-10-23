b929410e7b6fc3469ff5ad4bdc3da42a8fae1d3f
Daniel Mendoza <danieruone@gmail.com>
2024-10-22 19:35:54 -0500

    :sparkles: Add movements model

--- src/models/movement.tsx
+++ src/models/movement.tsx
@@ -1,1 +1,5 @@
-export interface Movement {}
+export interface Movement {
+  name: string;
+  category: string;
+  amount: number;
+}
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,8 +5,12 @@ import { View } from "react-native";
 import { CurrentBudgetHero } from "../components/CurrentBudgetHero";
 import { MovementList } from "../components/MovementList";
 
+// models
+import { Movement } from "../models/movement";
+
 const HomeScreen = () => {
   const [totalAmount, setTotalAmount] = useState<number>(0);
+  const [movementsList, setNovementsList] = useState<Movement[]>([]);
   return (
     <View>
       <CurrentBudgetHero totalAmount={totalAmount} />
