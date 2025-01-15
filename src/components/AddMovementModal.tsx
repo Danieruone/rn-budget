1a60149f126fd674a52da844c71f4bb3921a3ebb
Daniel Mendoza <danieruone@gmail.com>
2025-01-14 21:16:29 -0500

    Add modal comment

--- src/components/AddMovementModal.tsx
+++ src/components/AddMovementModal.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import { View, Text } from "react-native";
 
 export const AddMovementModal = () => {
+  // TODO: implement the modal
   return (
     <View>
-      <Text>Modal Test</Text>
+      <Text>Add your movement</Text>
     </View>
   );
 };
