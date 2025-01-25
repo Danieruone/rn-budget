0be2ebe72521a4f5de84adbd535b23354acd70d2
Daniel Mendoza <danieruone@gmail.com>
2025-01-25 13:24:12 -0500

    :sparkles: Add theme button

--- src/components/TitleContainer.tsx
+++ src/components/TitleContainer.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { useTheme } from "@react-navigation/native";
+import { ThemeButton } from "./shared/ThemeButton";
 
 export const TitleContainer = () => {
   const { colors } = useTheme();
   return (
     <View style={styles.container}>
       <Text style={[styles.title, { color: colors.primary }]}>My finances</Text>
+      <ThemeButton />
     </View>
   );
 };
@@ -15,6 +17,8 @@ const styles = StyleSheet.create({
   container: {
     marginTop: 20,
     paddingHorizontal: 20,
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   title: {
     fontSize: 30,
---
+++ src/components/shared/ThemeButton.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+import { CustomButton } from "./CustomButton";
+
+export const ThemeButton = () => {
+  // TODO: Implement the logic to change the theme
+  return <CustomButton title="Light" handlePress={() => {}} />;
+};
