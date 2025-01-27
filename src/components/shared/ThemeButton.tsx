1db01011ec576225784fa1dc3cb4be5def76ca85
Daniel Mendoza <danieruone@gmail.com>
2025-01-26 19:33:54 -0500

    :sparkles: Add icons to theme button

--- src/components/shared/ThemeButton.tsx
+++ src/components/shared/ThemeButton.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 
-import { CustomButton } from "./CustomButton";
+import { Pressable, View } from "react-native";
+
+// theme
 import { useTheme } from "../../themes/context/ThemeContext";
+
+// icons
+import IconLight from "react-native-vector-icons/Entypo";
+import IconDark from "react-native-vector-icons/MaterialIcons";
 
 export const ThemeButton = () => {
   const { toggleTheme, theme } = useTheme();
-  return <CustomButton title={theme} handlePress={toggleTheme} />;
+  return (
+    <Pressable onPress={toggleTheme}>
+      <View>
+        {theme === "light" ? (
+          <IconLight name="light-up" size={25} color={"black"} />
+        ) : (
+          <IconDark name="dark-mode" size={25} color={"white"} />
+        )}
+      </View>
+    </Pressable>
+  );
 };
